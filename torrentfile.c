84bcd3f2488a32f7743527ac2f98e08242012af6
AuroraOps04 <5239604@qq.com>
2024-12-16 17:00:21 +0800

    feat: get_infohash

--- torrentfile.c
+++ torrentfile.c
@@ -38,13 +38,66 @@ int find_keyword(const char *keyword, int *position) {
     if (torrentfile_content[i] == keyword[0] &&
         memcmp(&torrentfile_content[i], keyword, strlen(keyword)) == 0) {
       *position = i;
-      return 1;
+      return 0;
     }
   }
+  return -1;
+}
+int get_infohash() {
+  if (torrentfile_content == NULL) {
+    return -1;
+  }
+  int push_pop = 0;
+  int i, begin, end;
+  if (find_keyword("4:info", &i) < 0) {
+    return -1;
+  }
+  begin = i += 6;
+
+  while (i < torrentfile_size) {
+    if (torrentfile_content[i] == 'd') {
+      ++push_pop;
+      ++i;
+    } else if (torrentfile_content[i] == 'i') {
+      ++i;
+      if (i == torrentfile_size)
+        return -1;
+      while (torrentfile_content[i] != 'e') {
+        if (++i == torrentfile_size)
+          return -1;
+      }
+      ++i;
+      if (i == torrentfile_size)
+        return -1;
+    } else if (torrentfile_content[i] == 'l') {
+      ++push_pop;
+      ++i;
+    } else if (torrentfile_content[i] >= '0' && torrentfile_content[i] <= '9') {
+      int number = 0;
+      while (torrentfile_content[i] >= '0' && torrentfile_content[i] <= '9') {
+        ++i;
+        number = number * 10 + torrentfile_content[i] - '0';
+      }
+      i += number + 1;
+    } else if (torrentfile_content[i] == 'e') {
+      --push_pop;
+      if (push_pop == 0) {
+        end = i;
+        break;
+      }
+      ++i;
+    } else {
+      return -1;
+    }
+  }
+  if (i == torrentfile_size)
+    return -1;
+
   return 0;
 }
 
-void cleanup() {
+void release_torrentfile_memory() {
   torrentfile_size = 0;
-  free(torrentfile_content);
+  if (torrentfile_content != NULL)
+    free(torrentfile_content);
 }
--- torrentfile.h
+++ torrentfile.h
@@ -2,11 +2,13 @@
 #define TORRENTFILE_H
 extern char *torrentfile_content;
 extern long torrentfile_size;
+extern char info_hash[20];
 
 int read_torrentfile(const char *filename);
 
 int find_keyword(const char *keyword, int *position);
+int get_infohash();
 
-void cleanup();
+void release_torrentfile_memory();
 
 #endif // !TORRENTFILE_H
