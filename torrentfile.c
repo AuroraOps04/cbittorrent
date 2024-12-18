9cf8dd3930e78960f1989434e3ab1e21ceff1805
tory <5239604@qq.com>
2024-12-18 11:47:12 +0800

    get_filename get_announce_list

--- torrentfile.c
+++ torrentfile.c
@@ -1,9 +1,14 @@
 #include "torrentfile.h"
+#include <openssl/sha.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 long torrentfile_size = 0;
 char *torrentfile_content = NULL;
+char *filename = NULL;
+unsigned char info_hash[20];
+
+Announce_list *announce_list_head = NULL;
 
 int read_torrentfile(const char *filename) {
   if (filename == NULL) {
@@ -48,7 +53,7 @@ int get_infohash() {
     return -1;
   }
   int push_pop = 0;
-  int i, begin, end;
+  int i, begin, end = 0;
   if (find_keyword("4:info", &i) < 0) {
     return -1;
   }
@@ -75,8 +80,8 @@ int get_infohash() {
     } else if (torrentfile_content[i] >= '0' && torrentfile_content[i] <= '9') {
       int number = 0;
       while (torrentfile_content[i] >= '0' && torrentfile_content[i] <= '9') {
-        ++i;
         number = number * 10 + torrentfile_content[i] - '0';
+        ++i;
       }
       i += number + 1;
     } else if (torrentfile_content[i] == 'e') {
@@ -93,6 +98,101 @@ int get_infohash() {
   if (i == torrentfile_size)
     return -1;
 
+  SHA_CTX ctx;
+  SHA1_Init(&ctx);
+  SHA1_Update(&ctx, &torrentfile_content[begin], end - begin + 1);
+  SHA1_Final(info_hash, &ctx);
+
+  printf("info_hash: ");
+  for (int i = 0; i < 20; i++) {
+    printf("%02x", info_hash[i]);
+  }
+  printf("\n");
+
+  return 0;
+}
+
+int get_filename() {
+  if (torrentfile_content == NULL) {
+    return -1;
+  }
+  int p;
+  if (find_keyword("4:name", &p) < 0) {
+    return -1;
+  }
+  p += 6;
+  int number = 0;
+  while (torrentfile_content[p] != ':') {
+    number = number * 10 + torrentfile_content[p] - '0';
+    p++;
+  }
+  p++;
+  filename = (char *)malloc(number + 1);
+  memcpy(filename, &torrentfile_content[p], number);
+  filename[number] = 0;
+  return 0;
+}
+int get_announce_list() {
+  int p;
+  if (torrentfile_content == NULL) {
+    return -1;
+  }
+  if (find_keyword("13:announce-list", &p) >= 0) {
+    p += 16;
+    p++; // skip 'l'
+    while (torrentfile_content[p] != 'e') {
+      p++; // skip 'l'
+      int number = 0;
+      while (torrentfile_content[p] != ':') {
+        number = number * 10 + torrentfile_content[p] - '0';
+        p++;
+      }
+      p++; // skip ':'
+      Announce_list *node = (Announce_list *)malloc(sizeof(Announce_list));
+      node->next = NULL;
+      memcpy(node->announce, &torrentfile_content[p], number);
+      node->announce[number] = 0;
+
+      p += number;
+      p++; // skip 'e'
+
+      if (announce_list_head == NULL) {
+        announce_list_head = node;
+      } else {
+        Announce_list *q = announce_list_head;
+        while (q->next != NULL) {
+          q = q->next;
+        }
+        q->next = node;
+      }
+    }
+
+  } else {
+    if (find_keyword("8:announce", &p) < 0) {
+      return -1;
+    }
+
+    p += 10;
+    int number = 0;
+    while (torrentfile_content[p] != ':') {
+      number = number * 10 + torrentfile_content[p] - '0';
+      p++;
+    }
+    p++;
+
+    Announce_list *node = (Announce_list *)malloc(sizeof(Announce_list));
+    node->next = NULL;
+    memcpy(node->announce, &torrentfile_content[p], number);
+    node->announce[number] = 0;
+    announce_list_head = node;
+  }
+
+  Announce_list *q = announce_list_head;
+  while (q != NULL) {
+    printf("%s\n", q->announce);
+    q = q->next;
+  }
+
   return 0;
 }
 
@@ -100,4 +200,7 @@ void release_torrentfile_memory() {
   torrentfile_size = 0;
   if (torrentfile_content != NULL)
     free(torrentfile_content);
+
+  if (filename != NULL)
+    free(filename);
 }
--- torrentfile.h
+++ torrentfile.h
@@ -1,13 +1,17 @@
 #ifndef TORRENTFILE_H
 #define TORRENTFILE_H
-extern char *torrentfile_content;
-extern long torrentfile_size;
-extern char info_hash[20];
+
+typedef struct _Annouce_list {
+  char announce[128];
+  struct _Annouce_list *next;
+} Announce_list;
 
 int read_torrentfile(const char *filename);
 
 int find_keyword(const char *keyword, int *position);
 int get_infohash();
+int get_filename();
+int get_announce_list();
 
 void release_torrentfile_memory();
 
